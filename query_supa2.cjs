cee9b5340b3fffdd6928d3dfad27322e6109a378
yuri <yuri@MacBook-Air-de-yuri.local>
2026-02-20 09:23:16 -0300

    fix(pdf): removendo limitador e forcando expand

--- query_supa2.cjs
+++ query_supa2.cjs
@@ -3,13 +3,7 @@ const client = new Client({
   connectionString: 'postgresql://postgres:jBZr1SqcNt26nolO@db.nuzhtqxfasucecfmkwfw.supabase.co:5432/postgres',
   ssl: { rejectUnauthorized: false }
 });
-client.connect()
-  .then(() => client.query('SELECT id, name, latitude, longitude, size_ha FROM farms WHERE id = 7'))
-  .then(res => {
+client.connect().then(() => client.query('SELECT * FROM readings WHERE farm_id = 4 ORDER BY date DESC LIMIT 1')).then(res => {
     console.log(res.rows[0]);
     process.exit(0);
-  })
-  .catch(err => {
-    console.error(err);
-    process.exit(1);
-  });
+});
